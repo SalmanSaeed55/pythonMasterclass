beea8450d9fd9b724fdad1080cec9837cb43f515
SalmanSaeed55 <salmansaeed1359@gmail.com>
2026-05-14 11:14:39 +0100

    Continued with Section 5 Data Structures. Looked at list appending and enumerate function. Also completed a coding challenge from the course

--- 05_Data_Structures/02_buy_computer.py
+++ 05_Data_Structures/02_buy_computer.py
@@ -1,2 +1,19 @@
 current_choice = "-"
 computer_parts = []
+available_part  = ["Computer", "Monitor", "Keyboard", "Mouse", "Printer", "HDMI Cable", "DVD Drive"]
+# valid_choices = [str(i) for i in range(len(computer_parts) + 1)]
+valid_choices = []
+for i in range(len(available_part) + 1):
+    valid_choices.append(str(i))
+
+while current_choice != "0":
+    if current_choice in valid_choices:
+        print(f"Adding {available_part[int(current_choice) - 1]} to the list.")
+        computer_parts.append(available_part[int(current_choice) - 1])
+    else:
+        print("Please add options from the list below:")
+        for i, part in enumerate(available_part):
+            print(f"{i + 1}: {part}")
+    current_choice = input("> ")
+
+print(computer_parts)
---
+++ 05_Data_Structures/03_enumerate_example.py
@@ -0,0 +1,20 @@
+for index, character in enumerate("abcdefgh"):
+    print(f"{index + 1}: {character}")
+
+print()
+
+name = "Salman Saeed"
+for i, letter in enumerate(name):
+    print(f"{i + 1}: {letter}")
+
+print()
+
+value = 45237462
+for i, letter in enumerate(str(value)):
+    print(f"{i + 1}: {letter}")
+
+print()
+
+shopping_list = ["apple", "banana", "cherry", "strawberry", "mango"]
+for i, letter in enumerate(shopping_list):
+    print(f"{i + 1}: {letter}")
