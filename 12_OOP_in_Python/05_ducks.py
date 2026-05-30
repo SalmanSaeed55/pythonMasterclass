c0d3fa1cf2a102523c8d2792287c653487e22d45
SalmanSaeed55 <salmansaeed1359@gmail.com>
2026-05-30 13:16:09 +0100

    Completed Section 12 on Object-Oriented Programming Add Wing class and HTML document generation functionality

--- 12_OOP_in_Python/05_ducks.py
+++ 12_OOP_in_Python/05_ducks.py
@@ -1,4 +1,19 @@
+class Wing(object):
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        if self.ratio > 1.0:
+            print("Weee, this is fun")
+        elif self.ratio == 1.0:
+            print("This is hard work but I'm flying")
+        else:
+            print("I think I'll just walk")
+
 class Duck(object):
+
+    def __init__(self):
+        self._wing = Wing(1.8)
 
     def walk(self):
         print("waddle waddle waddle")
@@ -8,6 +23,9 @@ class Duck(object):
 
     def quack(self):
         print("quack quack")
+
+    def fly(self):
+        self._wing.fly()
 
 
 class Penguin(object):
@@ -22,15 +40,15 @@ class Penguin(object):
         print("Are you having a laugh? I don't swim")
 
 
-def test_duck(duck):
-    duck.walk()
-    duck.swim()
-    duck.quack()
+# def test_duck(duck):
+#     duck.walk()
+#     duck.swim()
+#     duck.quack()
 
 
 if __name__ == "__main__":
     donald = Duck()
-    test_duck(donald)
+    donald.fly()
 
-    percy = Penguin()
-    test_duck(percy)
+    # percy = Penguin()
+    # test_duck(percy)
